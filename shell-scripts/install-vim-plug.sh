704f62afb690a1161ef063e3aafb5cbb1c7ab7ec
Preocts <preocts@gmail.com>
2022-05-15 14:54:25 -0400

    setup vim with vim-plug

---
+++ shell-scripts/install-vim-plug.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+vimdot="../dotfiles/.vimrc"
+
+echo "Setting up vim..."
+
+# Download plug and place in .vim directory
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Ensure our vimrc is in place
+cp $vimdot ~
+
+# Run plug install from cli
+vim -es -u ~/.vimrc -i NONE -c "PlugInstall" -c "qa"
+
+echo "End of line."
