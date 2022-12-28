7e185f6decbb4aa8a8e634d9c2fbf3055209c360
Preocts <preocts@gmail.com>
2022-12-28 03:43:23 -0500

    brags

--- shell-scripts/install-braghook.sh
+++ shell-scripts/install-braghook.sh
@@ -2,7 +2,7 @@
 
 echo "Setting up braghook..."
 
-mkdir ~/braghook
+mkdir ~/brags
 curl https://raw.githubusercontent.com/Preocts/braghook/main/src/braghook/braghook.py -o ~/braghook.py
 
 echo "End of line."
