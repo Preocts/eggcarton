4485060795a112c1b801fc1d2e51a299d324981f
Preocts <preocts@preocts.com>
2023-02-03 20:56:44 -0500

    clean

--- install-shell-scripts.py
+++ install-shell-scripts.py
@@ -1,16 +1,32 @@
 import subprocess
-from pathlib import Path
 
-def install_shell_scripts():
-    # Get list of all files that start with "install-" from shell-scripts directory
-    shell_scripts = [x for x in Path("shell-scripts").iterdir() if x.name.startswith("install-")]
+RUN_ORDER = [
+    "install-dotfiles.sh",
+    "install-randoms.sh",
+    "install-vim-plug.sh",
+    "install-git.sh",
+    "install-pythons.sh",
+    "install-braghook.sh",
+    "install-npm_yarn.sh",
+    "install-go.sh",
+    # "install-1password.sh",
+    # "install-exercism-cli.sh",
+    # "install-vscode.sh",
+]
+
+
+def install_shell_scripts() -> int:
+    print("This script will run the following shell scripts:")
+    print("\n".join(RUN_ORDER))
+    if input("Continue? [y/N]") != "y":
+        return 1
 
     # Run each file
-    for shell_script in shell_scripts:
-        if input(f"Run {shell_script}? [Y/n]") == "n":
-            continue
+    for shell_script in RUN_ORDER:
         print(f"Running {shell_script}...")
-        subprocess.run(["bash", shell_script])
+        target = f"shell-scripts/{shell_script}"
+        subprocess.run(["bash", target])
+    return 0
 
 if __name__ == "__main__":
-    install_shell_scripts()
+    raise SystemExit(install_shell_scripts())
