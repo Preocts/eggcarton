22bf8e14e24da8d0479b4678bfe900a9efc81999
Preocts <preocts@preocts.com>
2023-04-02 14:05:58 -0400

    fix

--- shell-scripts/install-neovim.sh
+++ shell-scripts/install-neovim.sh
@@ -9,14 +9,15 @@ sudo apt-get install ninja-build gettext libtool-bin cmake g++ pkg-config unzip 
 echo "Clone repo and selecting stable branch for build"
 git clone https://github.com/neovim/neovim
 cd neovim
-get checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
+git checkout stable
+make CMAKE_BUILD_TYPE=Release
 
 echo "Installing Neovim with debug flags to default location /usr/local"
 sudo make install
 
 echo "Cleaning up"
 cd ..
-sudo rm -rf neovim
+rm -rf neovim
 
 echo "End of line."
 
