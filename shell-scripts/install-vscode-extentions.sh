30849fcc34d2b2625d6e9d6889c74681c6774a1d
Preocts <preocts@preocts.com>
2023-06-26 19:50:37 -0400

    Trim and update

--- shell-scripts/install-vscode-extentions.sh
+++ shell-scripts/install-vscode-extentions.sh
@@ -4,22 +4,15 @@
 # code --list-extensions | xargs -L 1 echo code --install-extension
 code --install-extension bungcip.better-toml
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension euskadi31.json-pretty-printer
 code --install-extension GitHub.copilot
 code --install-extension hashicorp.terraform
 code --install-extension janisdd.vscode-edit-csv
 code --install-extension magicstack.MagicPython
 code --install-extension mgmcdermott.vscode-language-babel
-code --install-extension ms-python.isort
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-toolsai.jupyter-keymap
-code --install-extension ms-toolsai.jupyter-renderers
-code --install-extension ms-toolsai.vscode-jupyter-cell-tags
-code --install-extension ms-toolsai.vscode-jupyter-slideshow
 code --install-extension oderwat.indent-rainbow
-code --install-extension redhat.vscode-commons
 code --install-extension redhat.vscode-yaml
+code --install-extension vscodevim.vim
 code --install-extension yzhang.markdown-all-in-one
 
