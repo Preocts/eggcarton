572da3c050159b89cc9ad4896a6dce37c6f11f2c
Preocts <preocts@preocts.com>
2023-07-30 15:10:06 -0400

    now even better toml

--- shell-scripts/install-vscode-extentions.sh
+++ shell-scripts/install-vscode-extentions.sh
@@ -2,7 +2,6 @@
 
 # Generated with:
 # code --list-extensions | xargs -L 1 echo code --install-extension
-code --install-extension bungcip.better-toml
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension GitHub.copilot
 code --install-extension hashicorp.terraform
@@ -13,6 +12,7 @@ code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension oderwat.indent-rainbow
 code --install-extension redhat.vscode-yaml
+code --install-extension tamasfe.even-better-toml
 code --install-extension vscodevim.vim
 code --install-extension yzhang.markdown-all-in-one
 
