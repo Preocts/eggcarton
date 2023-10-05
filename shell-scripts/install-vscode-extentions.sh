4860530a9720e2a466f46414f4eb524c42de973b
Preocts <preocts@preocts.com>
2023-10-05 13:58:52 -0400

    vscode work

--- shell-scripts/install-vscode-extentions.sh
+++ shell-scripts/install-vscode-extentions.sh
@@ -2,17 +2,29 @@
 
 # Generated with:
 # code --list-extensions | xargs -L 1 echo code --install-extension
+code --install-extension 4ops.terraform
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension equinusocio.vsc-material-theme-icons
 code --install-extension GitHub.copilot
+code --install-extension GitHub.copilot-chat
+code --install-extension golang.go
 code --install-extension hashicorp.terraform
 code --install-extension janisdd.vscode-edit-csv
 code --install-extension magicstack.MagicPython
-code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.flake8
+code --install-extension ms-python.mypy-type-checker
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.remote-explorer
 code --install-extension oderwat.indent-rainbow
+code --install-extension otovo-oss.htmx-tags
 code --install-extension redhat.vscode-yaml
+code --install-extension sdras.night-owl
+code --install-extension stkb.rewrap
 code --install-extension tamasfe.even-better-toml
-code --install-extension vscodevim.vim
+code --install-extension usernamehw.errorlens
 code --install-extension yzhang.markdown-all-in-one
-
