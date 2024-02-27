fa4475415bf218e6ccf5cf9f0e7324f0edfe12a5
Preocts <preocts@preocts.com>
2024-02-26 21:31:04 -0500

    extensions

--- shell-scripts/install-vscode-extentions.sh
+++ shell-scripts/install-vscode-extentions.sh
@@ -2,16 +2,18 @@
 
 # Generated with:
 # code --list-extensions | xargs -L 1 echo code --install-extension
-code --install-extension 4ops.terraform
+code --install-extension ops.terraform
+code --install-extension catppuccin.catppuccin-vsc
+code --install-extension catppuccin.catppuccin-vsc-icons
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension equinusocio.vsc-material-theme-icons
-code --install-extension GitHub.copilot
-code --install-extension GitHub.copilot-chat
+code --install-extension github.copilot
+code --install-extension github.copilot-chat
 code --install-extension golang.go
 code --install-extension hashicorp.terraform
 code --install-extension janisdd.vscode-edit-csv
-code --install-extension magicstack.MagicPython
+code --install-extension magicstack.magicpython
 code --install-extension ms-python.black-formatter
+code --install-extension ms-python.debugpy
 code --install-extension ms-python.flake8
 code --install-extension ms-python.mypy-type-checker
 code --install-extension ms-python.python
